acecafb4a99b4ad68863e0cdd706d08852f6e00e
Iqra <iqraazam4831@gmail.com>
2023-12-12 20:01:54 +0500

    initialize commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let b = document.body
+console.log("First child of b is ", b.firstChild)
+console.log("First  elemnt child of b is ", b.firstElementChild)
+let t = document.body.firstElementChild
+console.log(t)
+console.log(t.rows)
